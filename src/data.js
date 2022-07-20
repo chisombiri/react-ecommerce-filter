0e600a1f4f499111e5671b073350a76cb4da2a59
chisombiri <chisombiri@gmail.com>
2022-07-20 15:15:32 +0100

    initial commit and setup

---
+++ src/data.js
@@ -0,0 +1,89 @@
+// Products data
+
+export const products = [
+    {
+      id: 1,
+      title: "Gown Product",
+      category: "gown",
+      price: 15.99,
+      img: "./assets/6.png",
+    },
+    {
+      id: 2,
+      title: "Shirt Product",
+      category: "shirt",
+      price: 15.99,
+      img: "./assets/7.png",
+    },
+    {
+      id: 3,
+      title: "Shirt Product",
+      category: "shirt",
+      price: 15.99,
+      img: "./assets/8.png",
+    },
+    {
+      id: 4,
+      title: "Jewelry Product",
+      category: "jewelry",
+      price: 25.99,
+      img: "./assets/9.png",
+    },
+    {
+      id: 5,
+      title: "Jewelry Product",
+      category: "jewelry",
+      price: 15.99,
+      img: "./assets/10.png",
+    },
+    {
+      id: 6,
+      title: "Jewelry Product",
+      category: "jewelry",
+      price: 15.99,
+      img: "./assets/11.png",
+    },
+    {
+      id: 7,
+      title: "Watch Product",
+      category: "watch",
+      price: 15.99,
+      img: "./assets/1.png",
+    },
+    {
+      id: 8,
+      title: "Watch Product",
+      category: "watch",
+      price: 15.99,
+      img: "./assets/13.png",
+    },
+    {
+      id: 9,
+      title: "Shirt Product",
+      category: "shirt",
+      price: 35.99,
+      img: "./assets/5.png",
+    },
+    {
+      id: 10,
+      title: "Watch Product",
+      category: "watch",
+      price: 35.99,
+      img: "./assets/2.png",
+    },
+    {
+      id: 11,
+      title: "Shirt Product",
+      category: "shirt",
+      price: 35.99,
+      img: "./assets/3.png",
+    },
+    {
+      id: 12,
+      title: "Shirt Product",
+      category: "shirt",
+      price: 35.99,
+      img: "./assets/4.png",
+    },
+];
+  
